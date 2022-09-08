c34c65024960344411358052851a570fa6d87df2
MasanoriIwakura <hagobutler@yahoo.co.jp>
2022-09-08 10:05:50 +0900

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/MasanoriIwakura/go-camera-youtube
+
+go 1.19
+
+require gocv.io/x/gocv v0.31.0
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"gocv.io/x/gocv"
+)
+
+func main() {
+	webcam, err := gocv.VideoCaptureDevice(0)
+	if err != nil {
+		fmt.Println("Error: can not open camera.")
+		panic(err)
+	}
+
+	window := gocv.NewWindow("Hello")
+	img := gocv.NewMat()
+
+	for {
+		webcam.Read(&img)
+		window.IMShow(img)
+		window.WaitKey(1)
+	}
+}
